eb27c27c6c96c091984a7d589f96ae140c630da3
Agustin Striebeck <agustinstriebeck.it@gmail.com>
2024-02-15 10:58:59 -0300

    Etapa 2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 const baseCost = 1.16;
-let M2;
-let mf = 1.07;
-let insurancePrice
-
+let M2 = prompt("Ingrese los M2");
+let fm = 0;
+let insurancePrice;
+let dwellingType = prompt("Selecciona la vivienda a cotizar: \n" + "('Casa', 'P.H.', 'Dto. Edificio')");
 
 function calculateInsurance() {
 
-    prompt(M2);
-    insurancePrice = baseCost * M2 * mf;
-    alert("$"+ insurancePrice);
+    
+    switch(dwellingType) {
+        case "Casa":
+            fm = 1.009
+            break
+        case "P.H.":
+            fm = 1.005
+            break
+        case "Dto. Edificio":
+            fm = 1.002
+            break
+        default:
+            console.error("El tipo elegido no esta contemplado");
+    }
+    
+    if (fm > 1.000 && parseInt(M2)) {
+        insurancePrice = fm * M2 * baseCost;
+        console.log("Resultado de la Póliza: $ " + insurancePrice);
+    } else {
+        console.warn("Hubo un error en los datos ingresados.");
+    }
     
 }
+
+calculateInsurance();
 
