1c9587b7b6be076c75b3997404bf4b015dae21e8
Agustin Striebeck <agustinstriebeck.it@gmail.com>
2024-02-16 10:25:52 -0300

    Etapa 3

--- js/main.js
+++ js/main.js
@@ -1,34 +1,32 @@
 const baseCost = 1.16;
+let insurancePrice = 0;
 let M2 = prompt("Ingrese los M2");
-let fm = 0;
-let insurancePrice;
-let dwellingType = prompt("Selecciona la vivienda a cotizar: \n" + "('Casa', 'P.H.', 'Dto. Edificio')");
+let tDwellingType = prompt("Selecciona la vivienda a cotizar: \n ('Casa', 'P.H.', 'Dto. Edificio')");
+let tLocation = prompt("Selecciona la ubicación de la vivienda: \n ('CABA', 'Tandil', 'Costa Atlántica')")
+let propertyFM = 0;
+let locationFM = 0;
 
-function calculateInsurance() {
-
-    
-    switch(dwellingType) {
-        case "Casa":
-            fm = 1.009
-            break
-        case "P.H.":
-            fm = 1.005
-            break
-        case "Dto. Edificio":
-            fm = 1.002
-            break
-        default:
-            console.error("El tipo elegido no esta contemplado");
+if (tDwellingType != '') {
+    for (const dwellingType of propertyData){
+        if (propertyData.dwellingType === tDwellingType) {
+            propertyFM = pFactor;
+            break;
+        }
     }
-    
-    if (fm > 1.000 && parseInt(M2)) {
-        insurancePrice = fm * M2 * baseCost;
-        console.log("Resultado de la Póliza: $ " + insurancePrice);
-    } else {
-        console.warn("Hubo un error en los datos ingresados.");
-    }
-    
 }
 
-calculateInsurance();
+if (tLocation != '') {
+    for (const location of locationData) {
+        if (locationData.location === tLocation) {
+            locationFM = lFactor;
+            break;
+        }
+    }  
+}
 
+if (propertyFM && locationFM && parseInt(M2)) {
+    insurancePrice = propertyFM * locationFM * M2 * baseCost;
+    console.log("Resultado de la Póliza: $ " + insurancePrice);
+} else {
+    console.warn("Hubo un error en los datos ingresados.");
+}
---
+++ js/vars.js
@@ -0,0 +1,14 @@
+const propertyData   = [
+                        {dwellingType: 'Casa', pFactor: 1.009},
+                        {dwellingType: 'P.H.', pFactor: 1.005},
+                        {dwellingType: 'Depto. Edificio', pFactor: 1.002},
+                        {dwellingType: 'Barrio Privado', pFactor: 1.09},
+                        {dwellingType: 'Oficina', pFactor: 1.079},
+                        {dwellingType: 'Local Comercial', pFactor: 1.041},
+                        {dwellingType: 'Depósito Logística', pFactor: 1.072}];
+
+const locationData   = [
+                        {location: 'CABA', lFactor: 1.013},
+                        {location: 'Tandil', lFactor: 1.004},
+                        {location: 'Costa Atlántica', lFactor: 1.029},
+                        {location: 'Patagonia', lFactor: 1.000},];
